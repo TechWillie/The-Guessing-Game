e525ff93a1df6e6df3f5465a1af27a54ae3a73ef
TechWillie <140114388+TechWillie@users.noreply.github.com>
2024-02-11 04:30:37 -0500

    Game complete.. Needs Style, Plan to put on webiste..

--- The-Guessing-game.js
+++ The-Guessing-game.js
@@ -1,54 +1,75 @@
+const colors = require('colors')
+
+
 const readline = require("readline");
 // create an interface where we can talk to the user
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
+secretNuumber();
+function genNum(num1, num2){
+    let ranNum = Math.floor(Math.random()*num2)+ num1;
+    return ranNum
+}
 
-let secretNuumber = 23
+
+function secretNuumber(){
+    rl.question("Ok.. So. Give me a Low number...? ", lowNum => {
+        const lN = Number(lowNum)
+        rl.question(`Well.. The secret number will be between ${lN} and...? `, highNum => {
+            const hN = Number(highNum)
+            const randNum = genNum(lN, hN)
+            console.log(randNum);
+            askGuess()
+            function askGuess(){
+                rl.question("Before we begin, How many guesses you want...? ", amount =>{
+                    let _amount = Number(amount)
+                    rl.question("Please enter your first guess...? ", guess1 => {
+                        let guess = Number(guess1)
+                        console.log(`you guessed ${guess}`.bgGreen);
+                        // console.log(_amount);
+                        checkGuess(guess, _amount, randNum)
+                        return askGuess
+                    })
+                })
+
+            }
 
 
-function checkGuess(num, count = 0){
+
+        })
+    })
+    return secretNuumber
+}
+
+function checkGuess(num, count = 0, n){
     console.log(count);
     if(count === 1){
         console.log("Sorry the game is over and youy lost..");
         rl.close()
         return true
     }
-    if(num > secretNuumber){
-        // console.log("To High");
+    if(num > n){
+        console.log("To High");
         rl.question("Whooo boy.. You're a bit oveer. Plesase, Guess again...", guess1 => {
             let guess = Number(guess1)
             console.log(`OK.. You guessed ${guess}, and that's`);
-            checkGuess(guess, count-1)
+            checkGuess(guess, count-1, n)
         })
         return false
-    }else if(num < secretNuumber){
-        // console.log("To low");
+    }else if(num < n){
+        console.log("To low");
         rl.question("You thinking to small.. Bring it up a bit...", guess2 => {
             let guessw = Number(guess2)
-            console.log(`Alright, Let's see.. You guessed ${guessw}... which is`)
-            checkGuess(guessw, count-1)
+            console.log(`Alright, Let's see.. You guessed ${guessw}... which is`.red)
+            checkGuess(guessw, count-1, n)
         })
         return false
-    }else if(num === secretNuumber){
+    }else if(num === n){
         console.log("Correct");
         rl.close()
         return true
     };
-    return checkGuess
+return checkGuess
 };
-
-askGuess()
-function askGuess(){
-    rl.question("Before we begin, How many guesses you want...? ", amount =>{
-        let _amount = Number(amount)
-        rl.question("Please enter your first guess...?", guess1 => {
-            let guess = Number(guess1)
-            console.log(`you guessed ${guess}`);
-            // console.log(_amount);
-            checkGuess(guess, _amount)
-        })
-    })
-};
-return askGuess
